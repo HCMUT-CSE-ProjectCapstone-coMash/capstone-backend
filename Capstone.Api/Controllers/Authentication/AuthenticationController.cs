e052be2c710a2c928db50f7bd5cfa099cef11e00
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2026-02-07 18:28:29 +0700

    inital simple code

---
+++ Capstone.Api/Controllers/Authentication/AuthenticationController.cs
@@ -0,0 +1,21 @@
+using Capstone.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Api.Controllers.Authentication;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        return Ok(request);
+    }
+}
---
+++ Capstone.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,8 @@
+namespace Capstone.Contracts.Authentication;
+
+public record AuthenticationResponse(
+    Guid Id,
+    string FullName,
+    string Email,
+    string Token
+);
