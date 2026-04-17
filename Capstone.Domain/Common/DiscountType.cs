3d8a9595fb0bb43e45fecb6da8da6d3ba8e0d61c
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2026-04-18 00:38:26 +0700

    feature: add promotion table

---
+++ Capstone.Domain/Common/DiscountType.cs
@@ -0,0 +1,7 @@
+namespace Capstone.Domain.Common;
+
+public static class DiscountType
+{
+    public const string Percentage = "Percentage";
+    public const string Money = "Money";
+}
---
+++ Capstone.Domain/Common/PromotionStatus.cs
@@ -0,0 +1,8 @@
+namespace Capstone.Domain.Common;
+
+public static class PromotionStatus
+{
+    public const string Active = "Active";
+    public const string Inactive = "Inactive";
+    public const string Expired = "Expired";
+}
