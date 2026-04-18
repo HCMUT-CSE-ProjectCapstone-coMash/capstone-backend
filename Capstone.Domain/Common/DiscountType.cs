eb56d2dbdd039d08c7b71b1f5ae6d7a5efac311d
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2026-04-18 18:19:58 +0700

    fix: change discountType

--- Capstone.Domain/Common/DiscountType.cs
+++ Capstone.Domain/Common/DiscountType.cs
@@ -3,5 +3,5 @@ namespace Capstone.Domain.Common;
 public static class DiscountType
 {
     public const string Percentage = "Percentage";
-    public const string Money = "Money";
+    public const string Fixed = "Fixed";
 }
