4491b863c780c852406a91e23a832ac22638726d
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2026-05-04 14:17:53 +0700

    feature: token contain haschangedpassword

--- Capstone.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
+++ Capstone.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -2,5 +2,5 @@ namespace Capstone.Application.Common.Interfaces.Authentication;
 
 public interface IJwtTokenGenerator
 {
-    string GenerateToken(Guid userId, string fullName, string userRole);
+    string GenerateToken(Guid userId, string fullName, string userRole, bool HasChangedPassword);
 }
--- Capstone.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ Capstone.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -16,14 +16,15 @@ public class JwtTokenGenerator : IJwtTokenGenerator
         _jwtSettings = jwtSettings.Value;
     }
 
-    public string GenerateToken(Guid userId, string fullName, string userRole)
+    public string GenerateToken(Guid userId, string fullName, string userRole, bool HasChangedPassword)
     {
         var claims = new[]
         {
             new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
             new Claim(JwtRegisteredClaimNames.GivenName, fullName),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim("role", userRole)
+            new Claim("role", userRole),
+            new Claim("hasChangedPassword", HasChangedPassword.ToString())
         };
 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret));
