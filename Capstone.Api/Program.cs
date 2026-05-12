b84346298ed1eaedf6c8c794f7310234e47dee64
elfbe1306 <162111543+elfbe1306@users.noreply.github.com>
2026-05-12 18:26:40 +0700

    fix: cors

--- Capstone.Api/Program.cs
+++ Capstone.Api/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000", "http://localhost:8081", "https://capstone-frontend-nu-five.vercel.app")
+            .WithOrigins("http://localhost:3000", "http://localhost:8081", "https://comash.io.vn")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
